998f720a55f2ef8f8e1cd407e4b1f740952287a4
LUKAS A. MUELLER <millennium1412@yahoo.co.jp>
2016-02-04 15:38:21 +0100

    Adding Table View Cell

---
+++ BeeStats/BeeStats/FavoritePlayerTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  FavoritePlayerTableViewCell.swift
+//  BeeStats
+//
+//  Created by Lukas A. Mueller on 04/02/16.
+//  Copyright © 2016 Lukas A. Mueller. All rights reserved.
+//
+
+import UIKit
+
+class FavoritePlayerTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameTextLabel: UILabel!
+    @IBOutlet weak var rankNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
